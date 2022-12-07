94889383d6baf859d910dd23422fb13853d03314
nuru ibrahim <nuruibrahim11111@gmail.com>
2022-12-07 03:26:58 -0800

    add cv

--- src/component/header/CTA.jsx
+++ src/component/header/CTA.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-
-
+import React from 'react';
+import CV from '../../assets/cover-letter_resume.PDF';
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href="#portfolio"  className='btn'>show projects</a>
-        <a href="#contact" className="btn btn-primary">lets talk</a>
-
-       
+      <a href={CV} className="btn" download>
+        download my resume
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        lets talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
